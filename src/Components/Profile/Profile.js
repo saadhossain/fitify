dc679d2cf33f6cfd8f3941dcaaddc1b252dd765e
saadhossain <mshsarkar@gmail.com>
2022-09-29 18:28:42 +0600

    Break time added

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import userImage from '/public/profile.png';
 
 const Profile = (props) => {
     const time = props.data;
     const totalTime = time.reduce((prev, current) => prev + current, 0);
+
+    const [breakTime, setBreakTime] = useState(0);
+
+    const handleBreakTime = (time) => {
+        console.log('Button Clicked', props);
+        setBreakTime(time);
+    }
     return (
         <div className='sticky top-0'>
             {/* Username and Image */}
@@ -33,10 +40,10 @@ const Profile = (props) => {
             <div className='my-5'>
                 <h2 className='text-2xl'>Add a Break</h2>
                 <div className='flex items-center justify-between bg-[#F2F4FA] p-2 my-4 rounded'>
-                    {/* <h4 onClick={() => handleBreakTime(10)} className="text-md bg-white hover:bg-blue-600 hover:text-white rounded-3xl p-3 text-black cursor-pointer">10s</h4> */}
-                    <h4 className="text-md bg-white hover:bg-blue-600 hover:text-white rounded-3xl p-3 text-black cursor-pointer">20s</h4>
-                    <h4 className="text-md bg-white hover:bg-blue-600 hover:text-white rounded-3xl p-3 text-black cursor-pointer">30s</h4>
-                    <h4 className="text-md bg-white hover:bg-blue-600 hover:text-white rounded-3xl p-3 text-black cursor-pointer">40s</h4>
+                    <h4 onClick={() => handleBreakTime(10)} className="text-md bg-white hover:bg-blue-600 hover:text-white rounded-3xl p-3 text-black cursor-pointer">10s</h4>
+                    <h4 onClick={() => handleBreakTime(20)} className="text-md bg-white hover:bg-blue-600 hover:text-white rounded-3xl p-3 text-black cursor-pointer">20s</h4>
+                    <h4 onClick={() => handleBreakTime(30)} className="text-md bg-white hover:bg-blue-600 hover:text-white rounded-3xl p-3 text-black cursor-pointer">30s</h4>
+                    <h4 onClick={() => handleBreakTime(40)} className="text-md bg-white hover:bg-blue-600 hover:text-white rounded-3xl p-3 text-black cursor-pointer">40s</h4>
                 </div>
             </div>
             {/* Exercise Data */}
@@ -51,7 +58,7 @@ const Profile = (props) => {
                 <div className='bg-[#F2F4FA] p-2 my-4 rounded'>
                     <div className='flex items-center justify-between'>
                         <h3 className='text-xl text-black'>Break Time</h3>
-                        <p> Sec</p>
+                        <p>{breakTime} Seconds</p>
                     </div>
                 </div>
             </div>
